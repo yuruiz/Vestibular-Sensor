449e05f7e855358c6edbd7126d6b90afe3eb1e12
witan <accountregistor@gmail.com>
2014-04-09 14:34:15 -0400

    Both MPU and UART works!!

--- main.c
+++ main.c
@@ -33,25 +33,25 @@ static void Delay(unsigned int n)
 //System Initialization
 void InitSys()
 {
-        //use XT2 Oscillator
-        // BCSCTL1 &= ~XT2OFF; //open XT2 Oscillator
+    //use XT2 Oscillator
+    // BCSCTL1 &= ~XT2OFF; //open XT2 Oscillator
 
-        // do
-        // {
-        //     IFG1 &= ~OFIFG; //clear Oscillator flags
+    // do
+    // {
+    //     IFG1 &= ~OFIFG; //clear Oscillator flags
 
-        //     unsigned int iq0;
-        //     for (iq0 = 0xFF; iq0 > 0; iq0--); //delay, wait Oscillator to work
-        // }
-        // while ((IFG1 & OFIFG) != 0); //test if XT2 works
+    //     unsigned int iq0;
+    //     for (iq0 = 0xFF; iq0 > 0; iq0--); //delay, wait Oscillator to work
+    // }
+    // while ((IFG1 & OFIFG) != 0); //test if XT2 works
 
-        // BCSCTL2 = SELM_2 + SELS; //select XT2 for MCLK¡¢SMCLK
+    // BCSCTL2 = SELM_2 + SELS; //select XT2 for MCLK¡¢SMCLK
 
     //Other peripheral initialization
     InitIIC();
     MPU6500_Init();
 
-    // UartInit();
+    UartInit();
 
     //  _EINT(); //Open global interrupt control
 }
@@ -65,30 +65,39 @@ void main(void)
 
     // P1DIR |= 0x03;                        // Set P1.0 to output direction
 
-    if(!MPU_Test_Connection())
-        printf("Connection Error");
+    if (!MPU_Test_Connection())
+    { printf("Connection Error"); }
 
     for (;;)
     {
-        // SendUart("123456", 6);
+        // unsigned char strings[8] = "123456\r\n";
+        // SendUart(strings, 8);
+        writeBit(MPU6500_DEFAULT_ADDRESS, MPU6500_RA_PWR_MGMT_1, MPU6500_PWR1_SLEEP_BIT, 0);
 
         unsigned char buffer[14];
 
-        readBytes(MPU6500_DEFAULT_ADDRESS, MPU6500_RA_ACCEL_XOUT_H, 14, buffer);
+        ReadBytes(MPU6500_DEFAULT_ADDRESS, MPU6500_RA_ACCEL_XOUT_H, buffer, 14);
 
-        unsigned int ax = (((int16_t)buffer[0]) << 8) | buffer[1];
-        unsigned int ay = (((int16_t)buffer[2]) << 8) | buffer[3];
-        unsigned int az = (((int16_t)buffer[4]) << 8) | buffer[5];
-        unsigned int gx = (((int16_t)buffer[8]) << 8) | buffer[9];
-        unsigned int gy = (((int16_t)buffer[10]) << 8) | buffer[11];
-        unsigned int gz = (((int16_t)buffer[12]) << 8) | buffer[13];
+        unsigned int ax = (((unsigned int)buffer[0]) << 8) | buffer[1];
+        unsigned int ay = (((unsigned int)buffer[2]) << 8) | buffer[3];
+        unsigned int az = (((unsigned int)buffer[4]) << 8) | buffer[5];
+        //        unsigned int gx = (((unsigned int)buffer[8]) << 8) | buffer[9];
+        //        unsigned int gy = (((unsigned int)buffer[10]) << 8) | buffer[11];
+        //        unsigned int gz = (((unsigned int)buffer[12]) << 8) | buffer[13];
 
-        printf("%d\n", ax);
-        printf("%d\n", ay);
-        printf("%d\n", az);
-        printf("%d\n", gx);
-        printf("%d\n", gy);
-        printf("%d\n", gz);
+
+        unsigned char axs[10];
+        unsigned char ays[10];
+        unsigned char azs[10];
+        sprintf(axs, "ax: %d\r\n", ax);
+        sprintf(ays, "ay: %d\r\n", ay);
+        sprintf(azs, "az: %d\r\n", az);
+        SendUart(axs, 10);
+        SendUart(ays, 10);
+        SendUart(azs, 10);
+        // printf("%d\n", gx);
+        // printf("%d\n", gy);
+        // printf("%d\n", gz);
 
         // P1OUT ^= 0x03;                      // Toggle P1.0 using exclusive-OR
         // Delay(1000);
